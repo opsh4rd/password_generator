7346f96d0029e70193697900611221dd47c76995
Ruslan <miftakhov.ruslan.m@gmail.com>
2023-09-07 18:33:08 +0300

    Added everything into one function

--- generator/pass_random.py
+++ generator/pass_random.py
@@ -1,7 +1,19 @@
 import random
 
 
-def password_generator(*args):
+def password_generator():
+    # lists - ASCII
+    password_digit = (el for el in map(chr, list(range(33, 127))) if el.isdigit())
+    password_alpha_lower = (el for el in map(chr, list(range(97, 123))) if el.islower())
+    password_alpha_upper = (el for el in map(chr, list(range(65, 91))) if el.isupper())
+    password_special_characters = (el for el in '{@#}?%|*$~')
+
+    user_password_digits = input('Настройка генератора: \n' 'Добавить числа?: y/n ').lower()
+    user_small_letters = input('Добавить строчные символы?: y/n ').lower()
+    user_capital_letters = input('Добавить прописные символы?: y/n ').lower()
+    user_special_characters = input('Добавить специальные символы?: y/n ').lower()
+    user_password_length = int(input('Длина пароля?: '))
+
     value_list = []
     if user_password_digits == 'y':
         for el in password_digit:
@@ -21,19 +33,5 @@ def password_generator(*args):
     return result
 
 
-# lists - ASCII
-password_digit = (el for el in map(chr, list(range(33, 127))) if el.isdigit())
-password_alpha_lower = (el for el in map(chr, list(range(97, 123))) if el.islower())
-password_alpha_upper = (el for el in map(chr, list(range(65, 91))) if el.isupper())
-password_special_characters = (el for el in '{@#}?%|*$~')
-
 if __name__ == '__main__':
-    user_password_digits = input('Настройка генератора: \n'
-                                 'Добавить числа?: y/n ').lower()
-    user_small_letters = input('Добавить строчные символы?: y/n ').lower()
-    user_capital_letters = input('Добавить прописные символы?: y/n ').lower()
-    user_special_characters = input('Добавить специальные символы?: y/n ').lower()
-    user_password_length = int(input('Длина пароля?: '))
     print(password_generator())
-
-
