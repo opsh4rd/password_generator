8cb995c0d25179c900b8b751ce86201733277974
Ruslan <miftakhov.ruslan.m@gmail.com>
2023-09-08 21:32:12 +0300

    Added_exceptions

--- generator/pass_random.py
+++ generator/pass_random.py
@@ -1,37 +1,69 @@
 import random
+import time
 
 
 def password_generator():
-    # lists - ASCII
-    password_digit = (el for el in map(chr, list(range(33, 127))) if el.isdigit())
-    password_alpha_lower = (el for el in map(chr, list(range(97, 123))) if el.islower())
-    password_alpha_upper = (el for el in map(chr, list(range(65, 91))) if el.isupper())
-    password_special_characters = (el for el in '{@#}?%|*$~')
+    while True:
+        try:
+            # lists - ASCII
+            password_digit = (el for el in map(chr, list(range(33, 127))) if el.isdigit())
+            password_alpha_lower = (el for el in map(chr, list(range(97, 123))) if el.islower())
+            password_alpha_upper = (el for el in map(chr, list(range(65, 91))) if el.isupper())
+            password_special_characters = (el for el in '{@#}?%|*$~')
 
-    user_password_digits = input('Настройка генератора: \n' 'Добавить числа?: y/n ').lower()
-    user_small_letters = input('Добавить строчные символы?: y/n ').lower()
-    user_capital_letters = input('Добавить прописные символы?: y/n ').lower()
-    user_special_characters = input('Добавить специальные символы?: y/n ').lower()
-    user_password_length = int(input('Длина пароля?: '))
+            user_password_digits = input('Настройка генератора: \n' 'Добавить числа?: y/n ').lower()
+            if user_password_digits != 'y' and user_password_digits != 'n':
+                raise ValueError("Допустимые значения - 'y(yes)' или 'n(no)'")
 
-    value_list = []
-    if user_password_digits == 'y':
-        for el in password_digit:
-            value_list.append(el)
-    if user_small_letters == 'y':
-        for el in password_alpha_lower:
-            value_list.append(el)
-    if user_capital_letters == 'y':
-        for el in password_alpha_upper:
-            value_list.append(el)
-    if user_special_characters == 'y':
-        for el in password_special_characters:
-            value_list.append(el)
+            user_small_letters = input('Добавить строчные символы?: y/n ').lower()
+            if user_small_letters != 'y' and user_small_letters != 'n':
+                raise ValueError("Допустимые значения - 'y' или 'n'")
 
-    random_elements = random.choices(value_list, k=user_password_length)
-    result = ''.join(random_elements)
-    return result
+            user_capital_letters = input('Добавить прописные символы?: y/n ').lower()
+            if user_capital_letters != 'y' and user_capital_letters != 'n':
+                raise ValueError("Допустимые значения - 'y' или 'n'")
+
+            user_special_characters = input('Добавить специальные символы?: y/n ').lower()
+            if user_special_characters != 'y' and user_special_characters != 'n':
+                raise ValueError("Допустимые значения - 'y' или 'n'")
+
+            while True:
+                try:
+                    user_password_length = int(input('Длина пароля?: '))
+                    print(f'Происходит генерация пароля...')
+                    time.sleep(1.5)
+                    if user_password_length <= 0:
+                        raise ValueError("Графа должна быть положительным числом")
+                    break
+                except ValueError:
+                    print("Длина пароля должна быть положительным числом")
+                    continue
+
+
+            value_list = []
+            if user_password_digits == 'y':
+                for el in password_digit:
+                    value_list.append(el)
+            if user_small_letters == 'y':
+                for el in password_alpha_lower:
+                    value_list.append(el)
+            if user_capital_letters == 'y':
+                for el in password_alpha_upper:
+                    value_list.append(el)
+            if user_special_characters == 'y':
+                for el in password_special_characters:
+                    value_list.append(el)
+
+            random_elements = random.choices(value_list, k=user_password_length)
+            result = ''.join(random_elements)
+            return result
+
+        except ValueError as e:
+            print(f"Ошибка: {e}\nУкажите корректные значения.\n")
+            continue
 
 
 if __name__ == '__main__':
-    print(password_generator())
+    print(f'Ваш пароль - {password_generator()}')
+
+
